c5f4efc8775ce9a8094866a107b15b61e706f53c
Brian Looker <lookerb@uwosh.edu>
2015-10-14 14:58:58 -0500

    formatting changes

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 # Starts pyramid application
 # Created by David Hietpas (hietpasd@uwosh.edu)
 
-nohup ./python/bin/pserve production.ini --pid-file=lock.pid --log-file=event.log >> event.log &
+nohup ./env/bin/pserve production.ini --pid-file=lock.pid --log-file=event.log >> event.log &
